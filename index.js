78e4334f5f675c7243fb0355b573c282e4b9599e
Alexei Rusu <rusualexei1995@gmail.com>
2024-04-21 12:58:57 -0400

    basic animation loop

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ canvas.height = window.innerHeight
 class Player {
     constructor() {
         this.position = { x: 100, y: 100 }
+        this.velocity = { x: 0, y : 1}
         this.width = 30
         this.height = 30
     }
@@ -17,7 +18,22 @@ class Player {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+    }
 }
 
 const player = new Player()
-player.draw()
+player.update()
+
+
+// ainmation recursive loop
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+}   
+
+// animate()
